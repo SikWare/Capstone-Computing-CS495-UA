fa7966c006fbd02a7a778c5c8c2fdaaff95c54e5
apluth <Apluth@crimson.ua.edu>
2017-03-28 16:35:52 -0500

    basic

---
+++ FixMyLife/app/src/main/java/com/sikware/fixmylife/Item.java
@@ -0,0 +1,11 @@
+package com.sikware.fixmylife;
+
+/**
+ * Created by Aime1 on 3/28/2017.
+ */
+
+public class Item {
+
+
+
+}
---
+++ FixMyLife/app/src/main/java/com/sikware/fixmylife/User.java
@@ -0,0 +1,11 @@
+package com.sikware.fixmylife;
+
+/**
+ * Created by Adam Pluth on 3/28/2017.
+ */
+
+class User {
+
+    boolean admin;
+
+}
