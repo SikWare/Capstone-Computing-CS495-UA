f65667af4d6a2075a7d6e144ca23383efd9d71e0
apluth <Apluth@crimson.ua.edu>
2017-04-12 09:00:14 -0500

    attempting to fix login

--- FixMyLife/app/src/main/java/com/sikware/fixmylife/SplashScreen.java
+++ FixMyLife/app/src/main/java/com/sikware/fixmylife/SplashScreen.java
@@ -4,14 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this,MainActivity.class);
-        //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
     }
